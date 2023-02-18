6f6ba49002a881ba0703ad99d989169c21bcf1a5
Artem U <artemm3dlan2@gmail.com>
2023-02-18 21:06:01 +0400

    проет сломан

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import React from 'react';
 
 function Header(props) {
-  const history = useHistory();
-  const handleSignOut = () => {
-    localStorage.removeItem('jwt');
-    history.replace('/');
-  };
+  // const history = useHistory();
+  // const handleSignOut = () => {
+  //   localStorage.removeItem('jwt');
+  //   history.replace('/');
+  // };
 
   return (
     <header className="header">
       <div className="header__logo"></div>
       <button className='header__button'><a href='/sign-up'>{props.email}</a></button>
-      <button onClick={handleSignOut} className='header__button'>выход</button>
+      <button  className='header__button'>выход</button>
     </header>
   );
 }
