727231b7e502a8388b6f2573e0c186b741a3dff8
Artem U <artemm3dlan2@gmail.com>
2023-02-19 19:36:40 +0400

    rev2.2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,49 @@
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-
-import React from 'react';
+import { Link, Route, Routes } from "react-router-dom";
+import React from "react";
 
 function Header(props) {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-
-
+  
   return (
     <header className="header">
       <div className="header__logo"></div>
       {props.loggedIn ? (
         <div className="header__nav">
           <p className="header__email">{props.email}</p>
-          <Link className="header__button" to="/sign-in" onClick={props.handleSignOut}>
+          <Link
+            className="header__button"
+            to="/sign-in"
+            onClick={props.handleSignOut}
+          >
             Выйти
           </Link>
         </div>
-      ) : location.pathname === "/sign-up" ? (
-        <Link className="header__button" to="/sign-in" onClick={props.handleSignOut}>
-          Войти
-        </Link>
       ) : (
-        <Link className="header__button" to="/sign-up" onClick={props.handleSignOut}>
-          Регистрация
-        </Link>
+        <Routes>
+          <Route
+            path="/sign-up"
+            element={
+              <Link
+                className="header__button"
+                to="/sign-in"
+                onClick={props.handleSignOut}
+              >
+                Войти
+              </Link>
+            }
+          />
+          <Route
+            path="/sign-in"
+            element={
+              <Link
+                className="header__button"
+                to="/sign-up"
+                onClick={props.handleSignOut}
+              >
+                Регистрация
+              </Link>
+            }
+          />
+        </Routes>
       )}
     </header>
   );
